0bddc0b6af2d6df2510da704fc7c5437a1d44123
Sindri Már Kaldal Sigurjónsson <sindrikaldal@clients-pool-1.nat.ru.is>
2016-03-24 14:43:16 +0000

    flot

--- src/Game/Main.java
+++ src/Game/Main.java
@@ -8,12 +8,11 @@ import WordCollection.WordCollection;
  * Created by sindrikaldal on 23/03/16.
  */
 public class Main {
-    private static ScrabbleGUI gui;
-    private static WordCollection wordCollection;
+
+
 
     public static void main(String[] args) {
-        gui = new ScrabbleGUI(new Board());
-        wordCollection = new WordCollection();
+        Game game = new Game();
     }
 }
 
--- src/WordCollection/WordCollection.java
+++ src/WordCollection/WordCollection.java
@@ -3,30 +3,69 @@ package WordCollection;
 import FileReader.FileReader;
 import org.quinto.dawg.ModifiableDAWGSet;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
 /**
  * Created by sindrikaldal on 23/03/16.
  */
 public class WordCollection {
 
     private FileReader fileReader;
+    private HashSet<Letter> letters;
 
     private static ModifiableDAWGSet dawg;
 
     public WordCollection() {
 
         fileReader = new FileReader();
-
         dawg = new ModifiableDAWGSet();
+        letters = new HashSet<Letter>();
 
-        System.out.println("Reading word collection");
+        importWordCollection();
+        initalizeLetters();
+
+    }
+
+    private void importWordCollection() {
         fileReader.storeWordCollection();
-        System.out.println("Done reading word collection");
         dawg.addAll(fileReader.getLines());
+    }
 
-        System.out.println("Check if dawg list contains the word hestur");
+    private void initalizeLetters() {
 
-        if(dawg.contains("hestur")){
-            System.out.println("It does!");
-        }
+        letters.add(new Letter('A', 1));
+        letters.add(new Letter('Á', 3));
+        letters.add(new Letter('B', 5));
+        letters.add(new Letter('D', 5));
+        letters.add(new Letter('Ð', 2));
+        letters.add(new Letter('E', 3));
+        letters.add(new Letter('É', 7));
+        letters.add(new Letter('F', 3));
+        letters.add(new Letter('G', 3));
+        letters.add(new Letter('H', 4));
+        letters.add(new Letter('I', 1));
+        letters.add(new Letter('Í', 4));
+        letters.add(new Letter('J', 6));
+        letters.add(new Letter('K', 2));
+        letters.add(new Letter('L', 2));
+        letters.add(new Letter('M', 2));
+        letters.add(new Letter('N', 1));
+        letters.add(new Letter('O', 5));
+        letters.add(new Letter('Ó', 3));
+        letters.add(new Letter('P', 5));
+        letters.add(new Letter('R', 1));
+        letters.add(new Letter('S', 1));
+        letters.add(new Letter('T', 2));
+        letters.add(new Letter('U', 2));
+        letters.add(new Letter('Ú', 4));
+        letters.add(new Letter('V', 5));
+        letters.add(new Letter('X', 10));
+        letters.add(new Letter('Y', 6));
+        letters.add(new Letter('Ý', 5));
+        letters.add(new Letter('Þ', 7));
+        letters.add(new Letter('Æ', 4));
+        letters.add(new Letter('Ö', 6));
     }
 }
