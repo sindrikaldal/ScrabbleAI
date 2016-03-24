fb9478208d4af75f32e7c7b00efe8a2df4dec380
Eysteinn Gunnlaugsson <eysteinn13@ru.is>
2016-03-24 16:31:57 +0000

    flot

--- src/Player/HumanPlayer.java
+++ src/Player/HumanPlayer.java
@@ -1,8 +1,59 @@
 package Player;
 
+import Board.Board;
+import WordCollection.Letter;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by sindrikaldal on 23/03/16.
  */
 public class HumanPlayer implements Player {
+    int MAX_TILES_ON_HAND = 8;
+    List<Letter> currentTiles;
+    List<Integer> scoreHistory;
+    Board board;
+
+    public HumanPlayer(List<Letter> currentTiles, Board board) {
+        this.currentTiles = currentTiles;
+        this.scoreHistory = new ArrayList<Integer>();
+        this.board = board;
+    }
+
+    //region getters and setters
+    public int getMAX_TILES_ON_HAND() {
+        return MAX_TILES_ON_HAND;
+    }
+
+    public List<Letter> getCurrentTiles() {
+        return currentTiles;
+    }
+
+    public void setCurrentTiles(List<Letter> currentTiles) {
+        this.currentTiles = currentTiles;
+    }
+
+    public List<Integer> getScoreHistory() {
+        return scoreHistory;
+    }
+
+    public void setScoreHistory(List<Integer> scoreHistory) {
+        this.scoreHistory = scoreHistory;
+    }
+
+    public Board getBoard() {
+        return board;
+    }
+
+    public void setBoard(Board board) {
+        this.board = board;
+    }
+    //endregion getters an
+
+    @Override
+    public void makeMove() {
+        
+    }
 
 }
--- src/Player/Player.java
+++ src/Player/Player.java
@@ -1,8 +1,12 @@
 package Player;
 
+import WordCollection.Letter;
+
+import java.util.List;
+
 /**
  * Created by sindrikaldal on 23/03/16.
  */
 public interface Player {
-
+    public void makeMove ();
 }
