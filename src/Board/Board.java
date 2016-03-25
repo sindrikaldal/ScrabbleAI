6f903dd4a5aac3895b7bd2b28d51613b3e6a9e59
Sindri Már Kaldal Sigurjónsson <sindrikaldal@clients-pool-1.nat.ru.is>
2016-03-25 14:49:37 +0000

    starting to work on finding anchors

--- src/Board/Board.java
+++ src/Board/Board.java
@@ -12,6 +12,8 @@ public class Board {
         initializeBoard();
     }
 
+
+    /* Initalize the board. Assign the empty string and the approproate squaretype to each square */
     private void initializeBoard() {
         board = new Square[this.BOARD_SIZE][this.BOARD_SIZE];
 
@@ -19,82 +21,82 @@ public class Board {
             for(int j = 0; j < this.BOARD_SIZE; j++) {
                 if(i == 0 || i == 14) {
                     if(j % 7 == 0) {
-                        this.board[i][j] = new Square(SquareType.TRIPLE_WORD, "");
+                        this.board[i][j] = new Square(SquareType.TRIPLE_WORD, "", i, j);
                     }
                     else if(j == 3 || j == 11) {
-                        this.board[i][j] = new Square(SquareType.DOUBLE_LETTER, "");
+                        this.board[i][j] = new Square(SquareType.DOUBLE_LETTER, "", i, j);
                     }
                     else {
-                        this.board[i][j] = new Square(SquareType.NORMAL, "");
+                        this.board[i][j] = new Square(SquareType.NORMAL, "", i, j);
                     }
                 }
                 else if(i == 1 || i == 13) {
                     if(j == 1 || j == 13) {
-                        this.board[i][j] = new Square(SquareType.DOUBLE_WORD, "");
+                        this.board[i][j] = new Square(SquareType.DOUBLE_WORD, "", i, j);
                     }
                     else if(j == 5 || j == 9) {
-                        this.board[i][j] = new Square(SquareType.TRIPLE_LETTER, "");
+                        this.board[i][j] = new Square(SquareType.TRIPLE_LETTER, "", i, j);
                     }
                     else {
-                        this.board[i][j] = new Square(SquareType.NORMAL, "");
+                        this.board[i][j] = new Square(SquareType.NORMAL, "", i, j);
                     }
 
                 }
                 else if(i == 2 || i == 12) {
                     if(j == 2 || j == 12) {
-                        this.board[i][j] = new Square(SquareType.DOUBLE_WORD, "");
+                        this.board[i][j] = new Square(SquareType.DOUBLE_WORD, "", i, j);
                     }
                     else if(j == 6 || j == 8) {
-                        this.board[i][j] = new Square(SquareType.DOUBLE_LETTER, "");
+                        this.board[i][j] = new Square(SquareType.DOUBLE_LETTER, "", i, j);
                     }
                     else {
-                        this.board[i][j] = new Square(SquareType.NORMAL, "");
+                        this.board[i][j] = new Square(SquareType.NORMAL, "", i, j);
                     }
                 }
                 else if(i == 3 || i == 11) {
                     if(j % 7 == 0) {
-                        this.board[i][j] = new Square(SquareType.DOUBLE_LETTER, "");
+                        this.board[i][j] = new Square(SquareType.DOUBLE_LETTER, "", i, j);
                     }
                     else if(j == 3 || j == 11   ) {
-                        this.board[i][j] = new Square(SquareType.DOUBLE_WORD, "");
+                        this.board[i][j] = new Square(SquareType.DOUBLE_WORD, "", i, j);
                     }
                     else {
-                        this.board[i][j] = new Square(SquareType.NORMAL, "");
+                        this.board[i][j] = new Square(SquareType.NORMAL, "", i, j);
                     }
                 }
                 else if(i == 4 || i == 10) {
                     if(j == 4 || j == 10) {
-                        this.board[i][j] = new Square(SquareType.DOUBLE_WORD, "");
+                        this.board[i][j] = new Square(SquareType.DOUBLE_WORD, "", i, j);
                     }
                     else {
-                        this.board[i][j] = new Square(SquareType.NORMAL, "");
+                        this.board[i][j] = new Square(SquareType.NORMAL, "", i, j);
                     }
                 }
                 else if(i == 5 || i == 9) {
                     if( j == 1 || j == 5 || j == 9 || j == 13) {
-                        this.board[i][j] = new Square(SquareType.TRIPLE_LETTER, "");
+                        this.board[i][j] = new Square(SquareType.TRIPLE_LETTER, "", i, j);
                     }
                     else {
-                        this.board[i][j] = new Square(SquareType.NORMAL, "");
+                        this.board[i][j] = new Square(SquareType.NORMAL, "", i, j);
                     }
                 }
                 else if(i == 6 || i == 8) {
                     if( j == 2 || j == 6 || j == 8 || j == 12) {
-                        this.board[i][j] = new Square(SquareType.DOUBLE_LETTER, "");
+                        this.board[i][j] = new Square(SquareType.DOUBLE_LETTER, "", i, j);
                     }
                     else {
-                        this.board[i][j] = new Square(SquareType.NORMAL, "");
+                        this.board[i][j] = new Square(SquareType.NORMAL, "", i, j);
                     }
                 }
                 else if(i == 7) {
                     if(j == 0 || j == 14) {
-                        this.board[i][j] = new Square(SquareType.TRIPLE_WORD, "");
+                        this.board[i][j] = new Square(SquareType.TRIPLE_WORD, "", i, j);
                     }
                     else if(j == 7) {
-                        this.board[i][j] = new Square(SquareType.CENTER_SQUARE, "");
+                        this.board[i][j] = new Square(SquareType.CENTER_SQUARE, "", i, j);
                     }
                     else {
-                        this.board[i][j] = new Square(SquareType.NORMAL, "");
+                        this.board[i][j] = new Square(SquareType.NORMAL, "", i, j);
                     }
                 }
             }
@@ -107,5 +109,23 @@ public class Board {
 
     public Square[][] getBoard() {
         return board;
+    }
+
+    /* Check whether there are any squares adjacent to the given square that contain a letter */
+    public boolean hasAdjacentSquares(Square square) {
+
+        if(square.getX() > 0 && board[square.getX() - 1][square.getY()].getSquareType().equals(SquareType.CONTAINS_LETTER)) {
+            return true;
+        }
+        else if(square.getX() < (BOARD_SIZE - 1) && board[square.getX() + 1][square.getY()].getSquareType().equals(SquareType.CONTAINS_LETTER)) {
+            return true;
+        }
+        else if(square.getY() > 0 && board[square.getX()][square.getY() - 1].getSquareType().equals(SquareType.CONTAINS_LETTER)) {
+            return true;
+        }
+        else if(square.getY() < (BOARD_SIZE - 1) && board[square.getX()][square.getY() + 1].getSquareType().equals(SquareType.CONTAINS_LETTER)) {
+            return true;
+        }
+        return false;
     }
 }
--- src/Board/Square.java
+++ src/Board/Square.java
@@ -8,10 +8,16 @@ public class Square {
 
     private SquareType squareType;
     private String value;
+    private boolean isAnchor;
+    private int x;
+    private int y;
 
-    public Square(SquareType squareType, String value) {
+    public Square(SquareType squareType, String value, int x, int y) {
         this.squareType = squareType;
         this.value = value;
+        this.isAnchor = false;
+        this.x = x;
+        this.y = y;
     }
 
     public SquareType getSquareType() {
@@ -28,5 +34,28 @@ public class Square {
 
     public void setValue(String value) {
         this.value = value;
+    }
+    public boolean isAnchor() {
+        return isAnchor;
+    }
+
+    public void setAnchor(boolean anchor) {
+        isAnchor = anchor;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
     }
 }
